eb2703516e096ea824146e9fa4baca70370c12b4
asmindev <asmin987asmin@gmail.com>
2026-02-24 16:42:18 +0800

    fix: location ip

--- app/Http/Middleware/TrackVisitor.php
+++ app/Http/Middleware/TrackVisitor.php
@@ -6,6 +6,7 @@ use App\Models\Visitor;
 use Closure;
 use Illuminate\Http\Request;
 use Jenssegers\Agent\Agent;
+use Stevebauman\Location\Facades\Location;
 use Symfony\Component\HttpFoundation\Response;
 
 class TrackVisitor
@@ -37,6 +38,9 @@ class TrackVisitor
         $browser = $agent->browser() ?: null;
         $platform = $agent->platform() ?: null;
 
+        // Lookup geo location from IP
+        $location = Location::get($request->ip());
+
         Visitor::create([
             'ip_address' => $request->ip(),
             'path' => '/'.ltrim($request->path(), '/'),
@@ -46,9 +50,9 @@ class TrackVisitor
             'browser_version' => $browser ? ($agent->version($browser) ?: null) : null,
             'platform' => $platform,
             'platform_version' => $platform ? ($agent->version($platform) ?: null) : null,
-            'country' => null,
-            'country_code' => null,
-            'city' => null,
+            'country' => $location ? $location->countryName : null,
+            'country_code' => $location ? $location->countryCode : null,
+            'city' => $location ? $location->cityName : null,
             'referrer' => $request->header('referer'),
             'user_agent' => $request->userAgent() ?? '',
         ]);
